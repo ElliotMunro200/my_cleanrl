0a97e7a0a6024b2940b66085dc5c877f2ca7fc87
emunro <emunro@deep.uod.otago.ac.nz>
2022-05-09 16:39:11 +1200

    my first changes to the cleanrl repo

---
+++ benchmark/td3/mujoco_wandb_video.sh
@@ -0,0 +1,14 @@
+# HalfCheetah-v2
+poetry run python cleanrl/td3_continuous_action.py --env-id HalfCheetah-v2 --track --capture-video --seed 1 --wandb-project-name cleanrl
+poetry run python cleanrl/td3_continuous_action.py --env-id HalfCheetah-v2 --track --capture-video --seed 2 --wandb-project-name cleanrl
+poetry run python cleanrl/td3_continuous_action.py --env-id HalfCheetah-v2 --track --capture-video --seed 3 --wandb-project-name cleanrl
+
+# Walker2d-v2
+poetry run python cleanrl/td3_continuous_action.py --env-id Walker2d-v2 --track --capture-video --seed 1 --wandb-project-name cleanrl
+poetry run python cleanrl/td3_continuous_action.py --env-id Walker2d-v2 --track --capture-video --seed 2 --wandb-project-name cleanrl
+poetry run python cleanrl/td3_continuous_action.py --env-id Walker2d-v2 --track --capture-video --seed 3 --wandb-project-name cleanrl
+
+# Hopper-v2
+poetry run python cleanrl/td3_continuous_action.py --env-id Hopper-v2 --track --capture-video --seed 1 --wandb-project-name cleanrl
+poetry run python cleanrl/td3_continuous_action.py --env-id Hopper-v2 --track --capture-video --seed 2 --wandb-project-name cleanrl
+poetry run python cleanrl/td3_continuous_action.py --env-id Hopper-v2 --track --capture-video --seed 3 --wandb-project-name cleanrl
