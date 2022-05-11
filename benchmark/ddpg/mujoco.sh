89b5c8afaa4db85acaf0706e55bba9d5867acc16
ElliotMunro200 <74408400+ElliotMunro200@users.noreply.github.com>
2022-05-11 13:43:49 +1200

    file edits before trying to make local desktop and local Deep have the same origin: my_cleanrl repo

--- benchmark/ddpg/mujoco.sh
+++ benchmark/ddpg/mujoco.sh
@@ -1,14 +1,14 @@
 # HalfCheetah-v2
-poetry run python cleanrl/ddpg_continuous_action.py --env-id HalfCheetah-v2 --track --capture-video --seed 1 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
-poetry run python cleanrl/ddpg_continuous_action.py --env-id HalfCheetah-v2 --track --capture-video --seed 2 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
-poetry run python cleanrl/ddpg_continuous_action.py --env-id HalfCheetah-v2 --track --capture-video --seed 3 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+poetry run python cleanrl/ddpg_continuous_action.py --env-id HalfCheetah-v2 --seed 1 --wandb-project-name cleanrl
+poetry run python cleanrl/ddpg_continuous_action.py --env-id HalfCheetah-v2 --seed 2 --wandb-project-name cleanrl
+poetry run python cleanrl/ddpg_continuous_action.py --env-id HalfCheetah-v2 --seed 3 --wandb-project-name cleanrl
 
 # Walker2d-v2
-poetry run python cleanrl/ddpg_continuous_action.py --env-id Walker2d-v2 --track --capture-video --seed 1 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
-poetry run python cleanrl/ddpg_continuous_action.py --env-id Walker2d-v2 --track --capture-video --seed 2 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
-poetry run python cleanrl/ddpg_continuous_action.py --env-id Walker2d-v2 --track --capture-video --seed 3 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+poetry run python cleanrl/ddpg_continuous_action.py --env-id Walker2d-v2 --seed 1 --wandb-project-name cleanrl
+poetry run python cleanrl/ddpg_continuous_action.py --env-id Walker2d-v2 --seed 2 --wandb-project-name cleanrl
+poetry run python cleanrl/ddpg_continuous_action.py --env-id Walker2d-v2 --seed 3 --wandb-project-name cleanrl
 
 # Hopper-v2
-poetry run python cleanrl/ddpg_continuous_action.py --env-id Hopper-v2 --track --capture-video --seed 1 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
-poetry run python cleanrl/ddpg_continuous_action.py --env-id Hopper-v2 --track --capture-video --seed 2 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
-poetry run python cleanrl/ddpg_continuous_action.py --env-id Hopper-v2 --track --capture-video --seed 3 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+poetry run python cleanrl/ddpg_continuous_action.py --env-id Hopper-v2 --seed 1 --wandb-project-name cleanrl
+poetry run python cleanrl/ddpg_continuous_action.py --env-id Hopper-v2 --seed 2 --wandb-project-name cleanrl 
+poetry run python cleanrl/ddpg_continuous_action.py --env-id Hopper-v2 --seed 3 --wandb-project-name cleanrl
